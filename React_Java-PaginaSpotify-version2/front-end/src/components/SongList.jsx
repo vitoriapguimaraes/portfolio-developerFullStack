60db69227d28e39ad16400d6f7869608ceaa64b4
Vitória Guimarães <87993956+vitoriapguimaraes@users.noreply.github.com>
2025-02-16 14:21:37 -0300

    create new spotify react

---
+++ React_Java-PaginaSpotify-version2/front-end/src/components/SongList.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import SongItem from './SongItem';
+import { useState } from 'react';
+
+const SongList = ({ songsArray }) => {
+
+  const [items, setItems] = useState(5);
+
+  return (
+    <div className="song-list">
+      {songsArray
+        .filter((currentValue, index) => index < items)
+        .map((currentsSongObj, index) => (
+          <SongItem {...currentsSongObj} index={index} key={index} />
+        ))}
+
+      <p
+        className="song-list__see-more"
+        onClick={() => {
+          setItems(items + 5);
+        }}
+      >
+        Ver Mais
+      </p>
+
+    </div>
+  );
+};
+
+export default SongList;
